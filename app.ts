b491a726f601f6ce91d560f7bcd77c34b6cd19a8
Dương Tiến Vinh <64480713+DuckyMomo20012@users.noreply.github.com>
2023-01-30 23:03:39 +0700

    feat: migrate common files to ts

--- app.ts
+++ app.ts
@@ -1,9 +1,9 @@
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import express from 'express';
+import logger from 'morgan';
 
-const indexRouter = require('./routes/index');
+import indexRouter from './routes/index';
 
 const app = express();
 
@@ -19,4 +19,4 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 
-module.exports = app;
+export default app;
