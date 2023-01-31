d177619d86b61813787b04afb8b20710e910f83e
Dương Tiến Vinh <64480713+DuckyMomo20012@users.noreply.github.com>
2023-01-31 12:37:13 +0700

    refactor: new src dir

---
+++ src/app.ts
@@ -0,0 +1,24 @@
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import express from 'express';
+import logger from 'morgan';
+
+import usersRouter from './pages/api/users';
+import indexRouter from './pages/index';
+
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'pages'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, '../', 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+export default app;
